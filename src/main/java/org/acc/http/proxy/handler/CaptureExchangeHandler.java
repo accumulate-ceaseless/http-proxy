2be95aa7dc3016260510b69fe3cd8a99f7159295
accumulate-ceaseless <18519188909@163.com>
2020-07-20 15:51:44 +0800

    整理

--- src/main/java/org/acc/http/proxy/handler/CaptureExchangeHandler.java
+++ src/main/java/org/acc/http/proxy/handler/CaptureExchangeHandler.java
@@ -4,35 +4,22 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.ssl.SslContext;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 @Log4j2
 public class CaptureExchangeHandler extends ChannelInboundHandlerAdapter {
     private final Consumer<FullHttpRequest> consumer;
-    private final ChannelHandlerContext channelHandlerContext;
-    private final SslContext clientSslContext;
-
-    private AtomicInteger atomicInteger = new AtomicInteger();
-
-    private List<FullHttpRequest> fullHttpRequests = new ArrayList<>();
-
-    private Future<Channel> future;
+    private final List<FullHttpRequest> fullHttpRequests = new ArrayList<>();
+    private final Future<Channel> future;
 
 
-    public CaptureExchangeHandler(Consumer<FullHttpRequest> consumer,
-                                  ChannelHandlerContext channelHandlerContext,
-                                  SslContext clientSslContext,
-                                  Future<Channel> future) {
+    public CaptureExchangeHandler(Consumer<FullHttpRequest> consumer, Future<Channel> future) {
         this.consumer = consumer;
-        this.channelHandlerContext = channelHandlerContext;
-        this.clientSslContext = clientSslContext;
         this.future = future;
     }
 
